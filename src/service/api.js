02d436dc7f10b5897ea9e857d141780d38e194aa
Nguyen Le (Wang) <69196079+lecnguyenn@users.noreply.github.com>
2021-10-13 15:57:45 +0700

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = '329df5b6d17d0fa83cf768ee95c3d83a';
+
 
 
 export const  getData = async (city, country) =>{
